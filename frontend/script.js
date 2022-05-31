0ec90b3189b4ede410b30487e97f8d50872c626a
pbarna389 <64441402+pbarna389@users.noreply.github.com>
2022-05-31 11:33:12 +0200

    message added

---
+++ frontend/script.js
@@ -0,0 +1,31 @@
+const googleSearchButton = document.querySelector("#btn-google-search");
+
+console.log(googleSearchButton);
+
+const errorMessage = () => {
+    return `
+    <div class="error">
+        <p>Type in something please!</p>
+    </div>
+    `
+};
+
+const deleteMessage = () => {
+    const error = document.querySelector('.error');
+    error.parentNode.removeChild(error);
+}
+
+googleSearchButton.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const form = document.querySelector("#form-googleSearch");
+    const searchTerm = form.querySelector("input[name='q']").value;
+
+    if (searchTerm === '') {
+        form.insertAdjacentHTML('afterend', errorMessage());
+
+        setTimeout(deleteMessage, 3000);
+    } else {
+        form.submit();
+    };
+});
