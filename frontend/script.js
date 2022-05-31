3abda7d949ded3ce735d9ae6825013139c09c88b
pbarna389 <64441402+pbarna389@users.noreply.github.com>
2022-05-31 12:25:45 +0200

    Session end

--- frontend/script.js
+++ frontend/script.js
@@ -8,6 +8,26 @@ const errorMessage = () => {
     `
 };
 
+setInterval(save, 3000);
+
+function save() {
+    const textarea = document.querySelector('#doc-text');
+    const docText = textarea.value;
+
+    const saveText = document.querySelector('#save-in-progress-text');
+
+    saveText.classList.remove('hidden');
+
+    fetch('http://127.0.0.1:9000', {
+        method: 'POST',
+        body: docText
+    }).then(function () {
+        saveText.classList.add('hidden')
+    });
+
+};
+
+
 const deleteMessage = () => {
     const error = document.querySelector('.error');
     error.parentNode.removeChild(error);
